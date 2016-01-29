d6d1823fbd0a259e7f262564fcb28710f96b0248
Milos Sakovic <m@deva.co>
2016-01-29 13:53:20 +0100

    Fix a trailing quote

--- mac.bash
+++ mac.bash
@@ -8,4 +8,4 @@ alias profile="aa ~/Work/Data/bash_it-settings"
 alias deva="ssh root@host.deva.co"
 alias sites="atom ~/.homestead/Homestead.yaml /etc/hosts"
 alias vm='function __homestead() { (cd ~/Work/Vagrant/homestead/ && vagrant $*); unset -f __homestead; }; __homestead'
-alias sim="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app""
+alias sim="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
