504102e5b1db9971c451400aaf6caef26c68c286
Milos Sakovic <m@deva.co>
2016-02-07 00:22:47 +0100

    Change the folder for Homestead on mac

--- mac.bash
+++ mac.bash
@@ -8,5 +8,5 @@ alias profile="a ~/Work/Data/bash_it-settings/"
 alias deva="ssh root@host.deva.co"
 alias pi="ssh pi@pi.box"
 alias sites="atom ~/.homestead/Homestead.yaml /etc/hosts"
-alias vm='function __homestead() { (cd ~/Work/Vagrant/homestead/ && vagrant $*); unset -f __homestead; }; __homestead'
+alias vm='function __homestead() { (cd ~/Work/Vagrant/Homestead/ && vagrant $*); unset -f __homestead; }; __homestead'
 alias sim="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
