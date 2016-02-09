54cc0faa088d797c74f3c8b9be7ef3f91398737c
Milos Sakovic <m@deva.co>
2016-02-09 11:11:02 +0100

    Add aliases to traverse common directories on mac

--- mac.bash
+++ mac.bash
@@ -10,3 +10,10 @@ alias pi="ssh pi@pi.box"
 alias sites="atom ~/Work/Data/Homestead/Homestead.yaml /etc/hosts"
 alias vm='function __homestead() { (cd ~/Work/Data/Homestead/ && vagrant $*); unset -f __homestead; }; __homestead'
 alias sim="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
+
+# dirs
+alias s="cd ~/Work/_sandbox"
+alias gob="cd ~/Work/Data/bash_it-settings"
+
+# project dirs
+alias goa="cd ~/Work/_sandbox/aktivnosti"
