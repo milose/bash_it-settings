cafcdf29e36e1b94239b59e239ce3805d49e04df
Milos Sakovic <m@deva.co>
2016-10-12 19:32:03 +0200

    Change sites alias to just open /etc/hosts file

--- mac.bash
+++ mac.bash
@@ -7,7 +7,7 @@ alias aa="a ."
 alias profile="a ~/Work/Data/bash_it-settings/"
 alias deva="ssh -i ~/.ssh/do root@host.deva.co"
 alias pi="ssh pi@pi.box"
-alias sites="atom ~/Work/Data/Homestead/Homestead.yaml /etc/hosts"
+alias sites="atom /etc/hosts"
 alias vm='function __homestead() { (cd ~/Work/Data/Homestead/ && vagrant $*); unset -f __homestead; }; __homestead'
 alias sim="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
 
