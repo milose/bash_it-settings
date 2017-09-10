0faead96ae3774ca6cdbd154ae9ff03e78899ac3
Milos Sakovic <m@deva.co>
2017-09-10 10:47:07 +0200

    Add sleep and dnsflush aliases.

--- mac.bash
+++ mac.bash
@@ -11,8 +11,10 @@ alias hosts="atom /etc/hosts"
 alias vm='function __homestead() { (cd ~/Work/Data/Homestead/ && vagrant $*); unset -f __homestead; }; __homestead'
 alias sim="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
 alias wd="curl -s http://wttr.in/podgorica?lang=en | sed -n '1,7p'"
+alias sleep="pmset sleep now"
+alias dnsflush="sudo killall -HUP mDNSResponder"
 
-#go
+# shortcuts to folders
 alias s="cd ~/Work/_sandbox"
 alias gob="cd ~/Work/Data/bash_it-settings"
 alias goa="cd ~/Work/_sandbox/aktivnosti"
