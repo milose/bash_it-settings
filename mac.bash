631b8636efff337d22be77a24287df1871b3292e
Milos Sakovic <m@deva.co>
2017-11-09 19:51:37 +0100

    Capitalize town for weather.

--- mac.bash
+++ mac.bash
@@ -7,7 +7,7 @@ alias aa="atom ."
 alias hosts="atom /etc/hosts"
 alias vm='function __homestead() { (cd ~/Work/Data/Homestead/ && vagrant $*); unset -f __homestead; }; __homestead'
 alias sim="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
-alias wd="curl -s http://wttr.in/podgorica?lang=en | sed -n '1,7p'"
+alias wd="curl -s http://wttr.in/Podgorica?lang=en | sed -n '1,7p'"
 alias sleep="pmset sleep now"
 alias dnsflush="sudo killall -HUP mDNSResponder"
 
