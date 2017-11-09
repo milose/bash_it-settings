2fb2be44f938a765259b5cba2bb9fb49b270ff0b
Milos Sakovic <m@deva.co>
2017-11-09 19:51:51 +0100

    Add moon phase alias.

--- mac.bash
+++ mac.bash
@@ -8,6 +8,7 @@ alias hosts="atom /etc/hosts"
 alias vm='function __homestead() { (cd ~/Work/Data/Homestead/ && vagrant $*); unset -f __homestead; }; __homestead'
 alias sim="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
 alias wd="curl -s http://wttr.in/Podgorica?lang=en | sed -n '1,7p'"
+alias moon="curl -s wttr.in/Moon?lang=en | sed -n '1,23p'"
 alias sleep="pmset sleep now"
 alias dnsflush="sudo killall -HUP mDNSResponder"
 
