213a3fb609d59080949bb2412d1617557c92f428
Milos <m@deva.co>
2019-05-26 20:43:30 +0200

    Add color chart function.

--- custom.bash
+++ custom.bash
@@ -102,3 +102,14 @@ speed() {
 
     rm tempfile
 }
+
+# Display color chart
+
+colors() {
+    for i in {0..255} ; do
+        printf "\x1b[48;5;%sm%3d\e[0m " "$i" "$i"
+        if (( i == 15 )) || (( i > 15 )) && (( (i-15) % 6 == 0 )); then
+            printf "\n";
+        fi
+    done
+}
