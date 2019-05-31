3057d4fa59b32566b88b790d8d90384b9c113afd
Milos <m@deva.co>
2019-05-31 19:20:02 +0200

    Add random string generator function.

--- custom.bash
+++ custom.bash
@@ -81,6 +81,18 @@ alias trunc="truncate -s 0"
 
 
 # Functions
+
+# Random string generator usage `random [length]`
+random () {
+    LEN=32
+    ISNUM='^[0-9]+$'
+    if [[ $# -gt 0 && $1 =~ $ISNUM ]]; then
+        LEN=$1
+    fi
+    
+    echo "$(LC_ALL=C < /dev/urandom tr -dc A-Za-z0-9 | head -c $LEN)"
+}
+
 # Transfer function for transfer.sh
 transfer() { if [ $# -eq 0 ]; then echo -e "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi 
 tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; }
